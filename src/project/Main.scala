5e9337546a291295491850dba25c222fdf6dc5a2
Saluki <saluki@gmx.fr>
2015-12-07 21:32:20 +0100

    Multiple block updates

--- src/project/Main.scala
+++ src/project/Main.scala
@@ -1,12 +1,12 @@
 package project
 
 import JaCoP.scala._
+import JaCoP.constraints.PrimitiveConstraint
 
 object Main extends jacop {
-  var ectsValides = 0
   def main(args: Array[String]): Unit = {
 
-    type Course = (IntVar, Int, Int) //prend/prend pas, ects 
+    type Course = (IntVar, Int, Int)
 
     def coursMututellementExclusifs(cours1: IntVar, cours2: IntVar): Unit = {
       cours1 + cours2 < 2
@@ -16,54 +16,51 @@ object Main extends jacop {
       cours1.>=(cours2)
     }
 
-    def coursPrerequis(cours1: Course, cours2: Course, listeCoursValide: List[Course]): Unit = {
-      if (!listeCoursValide.contains(cours1)) {
-        cours2._1.==(0)
-      }
+    def coursPrerequis(cours1: Course, cours2: Course): Unit = {
+      coursCoRequis(cours1._1, cours2._1)
+      val tmp=IntVar("corequis;"+cours1._1.id()+";"+cours2._1.id(),0,1)
+      tmp.==(cours1._1.*(cours2._1))
+      tmp
     }
-    
-     def coursDejaValide(cours1: Course): Unit = {
-       cours1._1==(0)
-       ectsValides+=cours1._2
-     }
-    
 
-    /**
-     * cours(intvar, ects:int, bloc:int, dejavalide:int)
-     *
-     * Values de l intvar
-     * 0 = Cours jamais pris
-     * 1 = Veut prendre cours
-     *
-     */
-    val poo = (IntVar("poo", 0, 1), 2, 1) //todo: 60ects bloc1, 120 ects bloc 2
+    def coursDejaValide(cours1: Course): Unit = {
+      cours1._1 == (0)
+    }
+
+    val poo = (IntVar("poo", 0, 1), 1, 1) //todo: 60ects bloc1, 120 ects bloc 2
     val math = (IntVar("math", 0, 1), 1, 1)
-    val xml = (IntVar("xml", 0, 1), 3, 1)
-    val web = (IntVar("web", 0, 1), 2, 1)
-    val paoo = (IntVar("paoo", 0, 1), 2, 1)
-    val stage = (IntVar("stage", 0, 1), 1, 2)
-    val sec = (IntVar("sec", 0, 1), 2, 2)
-    val ihm = (IntVar("ihm", 0, 1), 3, 2)
+    val xml = (IntVar("xml", 0, 1), 1, 1)
+    val web = (IntVar("web", 0, 1), 1, 1)
+    val paoo = (IntVar("paoo", 0, 1), 4, 1)
+    val stage = (IntVar("stage", 0, 1), 4, 2)
+    val sec = (IntVar("sec", 0, 1), 1, 2)
+    val ihm = (IntVar("ihm", 0, 1), 1, 2)
     val paterns = (IntVar("paterns", 0, 1), 2, 2)
-    val nutrition = (IntVar("nutrition", 0, 1), 3, 2)
+    val nutrition = (IntVar("nutrition", 0, 1), 1, 2)
 
-    val listeCours = List(poo, math, xml, web, paoo, stage, sec, ihm, nutrition, paterns);
-
+    val listeCours = List(poo, math, xml, web, paoo, stage, sec, ihm, paterns, nutrition);
     val listeValeurOnPrend = listeCours map ((e) => e._1)
 
     //mettre cours deja valides dans une liste
-    val listeCoursValide = List()
+    val listeCoursValide = List(poo, math, web, nutrition, xml, ihm, nutrition, sec)
     listeCoursValide.foreach { cours =>
       coursDejaValide(cours)
     }
+    val ectsValides=listeCoursValide map (e => e._2) sum
+        
     //pre requis:
-    //paoo est prerequis de stage
-    coursPrerequis(paoo, stage, listeCoursValide);
-
+    //paoo est prerequis de stage TODO/verif si paoo est prerequis de stage et pas l inverse
+    val prerequis=Map((stage,paoo))
+    
+    val listePreToCo= for(key <- prerequis.keys) yield (coursPrerequis(key, prerequis(key)))
+    
     //co-requis
     //xmlS est co-requis de web
-    coursCoRequis(xml._1, web._1)
-
+    val corequis=Map((xml._1,stage._1))
+    corequis.keys.foreach{key =>
+      coursCoRequis(key, corequis(key))
+    }
+    
     //cours bloquants
     //sec et ihm sont des cours bloquants:
     coursMututellementExclusifs(sec._1, ihm._1);
@@ -76,21 +73,26 @@ object Main extends jacop {
       //ici on doit prendre tous les cours restqnts non pris
     } else {
       //contrainte 60ects progrqmme de cours etudiqnt
-      poo._1 * poo._2 + math._1 * math._2 + xml._1 * xml._2 + web._1 * web._2 + paoo._1 * paoo._2 + stage._1 * stage._2 + sec._1 * sec._2 + ihm._1 * ihm._2 + nutrition._1 * nutrition._2 + paterns._1 * paterns._2 > 6
+      sum(listeCours map (e => (e._1 * e._2))) >= 6
     }
-
-    val result = satisfy(search(listeValeurOnPrend, first_fail, indomain_middle))
+    
+    val result = minimize(search(listeValeurOnPrend, first_fail, indomain_middle),sum(listePreToCo.toList))
+    // val result = satisfy(search(listeValeurOnPrend, first_fail, indomain_middle))
 
     if (result) {
-      
-      println("SEMESTRE\nVous avez comme cours: ")
-      
-      listeCours.foreach { cours =>
-        
-        if(cours._1.value()==1) {
-        	println(cours._1.id())
-        }
+
+      println("SEMESTRE\nCours\tECTS")
+      //TODO: reunir cette ligne ci et lq suivante
+      val listeCoursPris = listeCours filter (e => e._1.value() == 1)
+
+      listeCoursPris.foreach { cours =>
+        println(cours._1.id() + "\t  " + cours._2)
       }
+      val totalEcts = listeCoursPris map (e => e._2) sum
+
+      println("TOTAL\t" + totalEcts);
+      println("nombre de prerequis vers corequis\t" + listePreToCo)
+
     } else println("Aucune solution")
 
   }
